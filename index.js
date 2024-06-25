82915cad615d791a48b0fb6e6a4e21b6d4da7198
80mahd08 <amarimahdi8@gmail.com>
2024-06-25 14:48:13 +0100

    version 1.0.5

--- index.js
+++ index.js
@@ -42,52 +42,23 @@ const cloneRepo = (branch, fileName) => {
 
 const installDependencies = (destinationPath) => {
 	return new Promise((resolve, reject) => {
-		const installCommand = "npm install"; // Use yarn install if you prefer yarn
-
 		console.log(chalk.green("Installing dependencies..."));
 		spinner.start();
 
-		const childProcess = exec(installCommand, {
-			shell: true,
-			cwd: destinationPath,
-		});
-		// Event handler for stdout data
-		childProcess.stdout.on("data", (data) => {
-			let out = data.toString().replace(/npm WARN/g, chalk.yellow("npm WARN"));
-			// Add more color patterns for potential messages here
-			// For example, replacing 'error' with red color
-			out = out.replace(/error/gi, chalk.red.bold("error"));
-			console.log(out);
-		});
+		// Change the current working directory to the destination path
+		process.chdir(destinationPath);
 
-		// Event handler for stderr data
-		childProcess.stderr.on("data", (data) => {
-			let out = data.toString().replace(/npm WARN/g, chalk.yellow("npm WARN"));
-			// Add more color patterns for potential messages here
-			// For example, replacing 'error' with red color
-			out = out.replace(/error/gi, chalk.red.bold("error"));
-			console.error(out);
-		});
-
-		childProcess.on("error", (err) => {
-			reject(
-				new Error(
-					`Error occurred while installing dependencies: ${err.message}`
-				)
-			);
-		});
-
-		childProcess.on("close", (code) => {
+		exec("npm install", (err, stdout, stderr) => {
 			spinner.stop();
-			if (code === 0) {
-				console.log(chalk.green("Dependencies installed successfully"));
-				resolve();
-			} else {
+			if (err) {
 				reject(
 					new Error(
-						`Error occurred while installing dependencies. Exit code: ${code}`
+						`Error occurred while installing dependencies: ${err.message}`
 					)
 				);
+			} else {
+				console.log(chalk.green("Dependencies installed successfully"));
+				resolve();
 			}
 		});
 	});
