1ed1e59d12547a192c2c46b49b31a3ddbe0dfca7
80mahd08 <amarimahdi8@gmail.com>
2024-06-25 14:37:47 +0100

    make changes

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 
 import chalk from "chalk";
 import inquirer from "inquirer";
-import { createSpinner } from "nanospinner";
 import { exec } from "child_process";
 import path from "path";
 import { fileURLToPath } from "url";
+import { createSpinner } from "nanospinner";
 
 const spinner = createSpinner();
 
@@ -41,20 +41,49 @@ const cloneRepo = (branch, fileName) => {
 
 const installDependencies = (destinationPath) => {
 	return new Promise((resolve, reject) => {
+		const installCommand = "npm install"; // Use yarn install if you prefer yarn
+
 		console.log(chalk.green("Installing dependencies..."));
 		spinner.start();
 
-		exec(`cd ${destinationPath} && npm install`, (err, stdout, stderr) => {
+		const childProcess = exec(installCommand, { cwd: destinationPath });
+		// Event handler for stdout data
+		childProcess.stdout.on("data", (data) => {
+			let out = data.toString().replace(/npm WARN/g, chalk.yellow("npm WARN"));
+			// Add more color patterns for potential messages here
+			// For example, replacing 'error' with red color
+			out = out.replace(/error/gi, chalk.red.bold("error"));
+			console.log(out);
+		});
+
+		// Event handler for stderr data
+		childProcess.stderr.on("data", (data) => {
+			let out = data.toString().replace(/npm WARN/g, chalk.yellow("npm WARN"));
+			// Add more color patterns for potential messages here
+			// For example, replacing 'error' with red color
+			out = out.replace(/error/gi, chalk.red.bold("error"));
+			console.error(out);
+		});
+
+		childProcess.on("error", (err) => {
+			reject(
+				new Error(
+					`Error occurred while installing dependencies: ${err.message}`
+				)
+			);
+		});
+
+		childProcess.on("close", (code) => {
 			spinner.stop();
-			if (err) {
-				reject(
-					new Error(
-						`Error occurred while installing dependencies: ${err.message}`
-					)
-				);
-			} else {
+			if (code === 0) {
 				console.log(chalk.green("Dependencies installed successfully"));
 				resolve();
+			} else {
+				reject(
+					new Error(
+						`Error occurred while installing dependencies. Exit code: ${code}`
+					)
+				);
 			}
 		});
 	});
